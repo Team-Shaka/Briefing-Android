14545b8267437daa12d510882c90df6b830426b8
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-05-20 15:37:26 +0900

    feat: Add missing compose ui-tooling dependency

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -12,4 +12,6 @@ dependencies {
     api(libs.androidx.compose.material3)
     api(libs.androidx.compose.foundation)
     api(libs.androidx.compose.ui.tooling.preview)
+
+    debugApi(libs.androidx.compose.ui.tooling)
 }
